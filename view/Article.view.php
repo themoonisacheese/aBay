09cb0869428909f9a95f8c4af39376caa2d0bf96
emPiet <emilien.piet@gmail.com>
2018-11-03 02:12:29 +0100

    Vues

--- view/Article.view.php
+++ view/Article.view.php
@@ -8,14 +8,8 @@
     <?php
     $article = getArticle($GET_['ref']);
      ?>
-<nav>
-  <ul>
-    <li>un truc</li>
-    <li>un autre truc</li>
-    <li>un troisieme</li>
-    <li>se connecter</li>
-  </ul>
-</nav>
+
+
 
 <div class="article">
     <div class="photo"><img src="<?= $article->image ?>" alt=""></div>
@@ -25,6 +19,10 @@
       <h2>Prix:</h2><p><?= $article->prix ?></p>
     </div>
 </div>
+
+
+
+
 
 
   </body>
---
+++ view/Footer.view.php
@@ -0,0 +1,7 @@
+
+</div>
+<footer>
+  <p>Copyright (c) <  TeamPhPForever 2019 Copyright Holder All Rights Reserved.</p>
+</footer>
+  </body>
+</html>
